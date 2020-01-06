50326d4f608b743f102fd6e47c2c34aef663519e
Tim Pietz <tim@pietz.me>
2020-01-06 13:26:13 +0100

    Fetch the API key from `.env`

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # nyt-scraper
 Scrape article metadata comments from NYTimes
+
+## Setup
+1. Make a copy of `.env.example` and name it `.env`
+1. Replace the values in `.env` as needed
---
+++ run.py
@@ -0,0 +1,11 @@
+import os
+
+from nyt_scraper import run_scraper
+
+from dotenv import load_dotenv
+
+if __name__ == '__main__':
+    load_dotenv()
+
+    api_key = os.environ.get('NYT_API_KEY')
+    run_scraper(api_key)
