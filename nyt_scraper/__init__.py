d34220ee63c62891ec7fe704baa552c950eb3e8e
Tim Pietz <tim@pietz.me>
2020-01-06 13:12:01 +0100

    Implement full and by-month scraping functions which optionally store the results

---
+++ nyt_scraper/__init__.py
@@ -0,0 +1,1 @@
+from nyt_scraper.scraper import run_scraper, scrape_month
---
+++ nyt_scraper/scraper.py
@@ -0,0 +1,65 @@
+import datetime as dt
+from pathlib import Path
+from typing import Tuple, Callable
+
+import pandas as pd
+
+from nyt_scraper.articles import fetch_articles_by_month, articles_to_df
+from nyt_scraper.comments import fetch_comments, comments_to_df
+from nyt_scraper.nyt_api import NytApi
+
+
+out_dir = Path.cwd() / 'data'
+
+
+def run_scraper(api_key: str):
+    """Scrape articles and comments month by month, starting from the current month"""
+
+    # the year and month to be fetched
+    date = dt.datetime.now().date().replace(day=1)
+    while True:
+        scrape_month(api_key, date)
+
+        # go one month back
+        date = (date - dt.timedelta(days=1)).replace(day=1)
+
+
+def scrape_month(api_key: str, date: dt.date, force_fetch: bool = False, store: bool = True) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    """Scrape articles and comments for a given month. The `date.day` is ignored."""
+
+    api = NytApi(api_key)
+
+    article_df = cached(
+        fetch=lambda: articles_to_df(fetch_articles_by_month(api, date)),
+        file=out_file(date, 'articles'),
+        force_fetch=force_fetch,
+        store=store,
+    )
+
+    article_ids_and_urls = list(article_df['web_url'].iteritems())
+    comment_df = cached(
+        fetch=lambda: comments_to_df(fetch_comments(api, article_ids_and_urls)),
+        file=out_file(date, 'comments'),
+        force_fetch=force_fetch,
+        store=store,
+    )
+
+    return article_df, comment_df
+
+
+def cached(file: Path, fetch: Callable[[], pd.DataFrame], force_fetch: bool, store: bool) -> pd.DataFrame:
+    if not file.exists() or force_fetch:
+        df = fetch()
+
+        if store:
+            file.parent.mkdir(exist_ok=True)
+            df.to_pickle(str(file))
+
+        return df
+    else:
+        return pd.read_pickle(str(file))
+
+
+def out_file(date: dt.date, kind: str) -> Path:
+    return out_dir / f'{date.year}-{date.month:02d}-{kind}.pickle'
+
