e09cf037c50633a4c967d27363d4633594fa37bb
benjamin-awd <benjamindornel@gmail.com>
2021-01-05 09:53:18 +0800

    Update depreciated function

--- nytimes_scraper/articles/postprocessing.py
+++ nytimes_scraper/articles/postprocessing.py
@@ -1,11 +1,10 @@
 from typing import List, Dict
 
 import pandas as pd
-import pandas.io.json
 
 
 def articles_to_df(articles: List[Dict]) -> pd.DataFrame:
-    df = pd.io.json.json_normalize(articles).set_index('_id')
+    df = pd.json_normalize(articles).set_index('_id')
 
     if 'pub_date' in df:
         df['pub_date'] = pd.to_datetime(df['pub_date'])
